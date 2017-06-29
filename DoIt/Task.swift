2c6d5c70ca0bd273be52fc7290b111a688c4e911
jdlitz <jeffreydl70@gmail.com>
2017-06-28 21:15:03 -0400

    Second Commit

---
+++ DoIt/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Jeffrey Litzinger on 6/26/17.
+//  Copyright © 2017 Jeffrey Litzinger, MD. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+    
+    
+}
